37c1df42dfd9f719377fa6d6508678356febf728
Youngho Cho <dominoyh5@gmail.com>
2024-03-27 19:52:21 +0900

    update: add timer callback

--- pico.c
+++ pico.c
@@ -5,6 +5,17 @@
 #define ADC_PIN 26
 #define ADC_NUM 0
 
+bool timer_callback(struct repeating_timer *t) {
+	char c[2];
+
+	uint adc_raw = adc_read();
+	c[0] = adc_raw >> 8;
+	c[1] = adc_raw;
+	write(1, c, 2);
+
+	return true;
+}
+
 int main() {
 	stdio_init_all();
 	stdio_set_translate_crlf(&stdio_usb, false);
@@ -13,19 +24,10 @@ int main() {
 	adc_gpio_init(ADC_PIN);
 	adc_select_input(ADC_NUM);
 
-	while (true) {
-		char c[2];
+	struct repeating_timer timer;
+	add_repeating_timer_us(250, timer_callback, NULL, &timer);
 
-		uint adc_raw = adc_read();
-
-		// printf("%d\n", adc_raw);
-
-		c[0] = adc_raw >> 8;
-		c[1] = adc_raw;
-		write(1, c, 2);
-
-		// sleep_ms(1);
-		sleep_us(250);
-		// sleep_us(125);
+	while (1) {
+		tight_loop_contents();
 	}
 }
